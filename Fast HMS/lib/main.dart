8a81250029ec7a346ccb402c8879d12fbc00e548
Basil Ali Khan <63046413+BasilKhan07@users.noreply.github.com>
2022-12-19 20:53:14 +0500

    Update main.dart

--- Fast HMS/lib/main.dart
+++ Fast HMS/lib/main.dart
@@ -6,9 +6,9 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://rxsqmwhuophzzhpqnvgq.supabase.co',
+    url: 'YOUR URL',
     anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4c3Ftd2h1b3BoenpocHFudmdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg2OTQzMDIsImV4cCI6MTk4NDI3MDMwMn0.N6bnlxBTgurEa4WLrMnLDuanpv5KGGHLMwPmBSpxGes',
+        'YOUR ANONKEY',
   );
   runApp(
     const MyApp(),
